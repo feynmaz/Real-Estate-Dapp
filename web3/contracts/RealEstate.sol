7b11982ccdb0c9b60d174de46b0f6f007083c9ec
Nikolai Mazein <feynmaz@gmail.com>
2024-05-11 11:18:28 +0500

    implement listProperty

--- web3/contracts/RealEstate.sol
+++ web3/contracts/RealEstate.sol
@@ -2,16 +2,17 @@
 pragma solidity ^0.8.9;
 
 contract RealEstate {
-    // ---- Property ---- 
+    // ---- Property ----
     // State
     struct Property {
-        uint256 productId;
+        uint256 productID;
         address owner;
         uint256 price;
         string propertyTitle;
         string category;
         string images;
         string propertyAddress;
+        string description;
         address[] reviewers;
         string[] reviews;
     }
@@ -21,9 +22,23 @@ contract RealEstate {
     uint256 public propertyIndex;
 
     // Events
-    event PropertyListed(uint256 indexed id, address indexed owner, uint256 price);
-    event PropertySold(uint256 indexed id, address indexed oldOwner, address indexed newOwner, uint256 price);
-    event PropertyResold(uint256 indexed id, address indexed oldOwner, address indexed newOwner, uint256 price);
+    event PropertyListed(
+        uint256 indexed id,
+        address indexed owner,
+        uint256 price
+    );
+    event PropertySold(
+        uint256 indexed id,
+        address indexed oldOwner,
+        address indexed newOwner,
+        uint256 price
+    );
+    event PropertyResold(
+        uint256 indexed id,
+        address indexed oldOwner,
+        address indexed newOwner,
+        uint256 price
+    );
 
     // ---- Review ----
     // State
@@ -35,7 +50,7 @@ contract RealEstate {
         uint256 likes;
     }
 
-    struct Product{
+    struct Product {
         uint256 productId;
         uint256 totalRating;
         uint256 numReviews;
@@ -49,12 +64,45 @@ contract RealEstate {
     mapping(uint256 => Product) private products;
 
     // Events
-    event ReviewAdded(uint256 indexed productId, address indexed reviewer, uint256 rating, string commment);
-    event ReviewLiked(uint256 indexed productId, uint256 indexed reviewIndex, address indexed liker, uint256 likes);
-
+    event ReviewAdded(
+        uint256 indexed productId,
+        address indexed reviewer,
+        uint256 rating,
+        string commment
+    );
+    event ReviewLiked(
+        uint256 indexed productId,
+        uint256 indexed reviewIndex,
+        address indexed liker,
+        uint256 likes
+    );
 
     // Property functions
-    function listProperty() external returns (uint256) {}
+    function listProperty(
+        address owner,
+        uint256 price,
+        string memory _propertyTitle,
+        string memory _category,
+        string memory _images,
+        string memory _propertyAddress,
+        string memory _description
+    ) external returns (uint256) {
+        require(price > 0, "Price must be greater than 0.");
+
+        uint256 productId = propertyIndex++;
+        Property storage property = properties[productId];
+
+        property.productID = productId;
+        property.owner = owner;
+        property.price = price;
+        property.propertyTitle = _propertyTitle;
+        property.category = _category;
+        property.images = _images;
+        property.propertyAddress = _propertyAddress;
+        property.description = _description;
+
+        emit PropertyListed(productId, owner, price);
+    }
 
     function updateProperty() external returns (uint256) {}
 
