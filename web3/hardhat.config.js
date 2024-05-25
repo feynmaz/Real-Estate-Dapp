4733da49f96d15da0a05133a1400269be2e8c82c
Nikolai Mazein <feynmaz@gmail.com>
2024-05-25 12:03:01 +0500

    set up hardhat for deploy

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,5 +1,20 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const RPC_URL = "https://rpc.ankr.com/polygon_amoy"
+
 module.exports = {
+  defaultNetwork: "polygon_amoy",
+  networks: {
+    hardhat: {
+      chainId: 80002,
+    },
+    polygon_amoy: {
+      url: RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+
   solidity: {
     version: "0.8.17",
     settings: {
